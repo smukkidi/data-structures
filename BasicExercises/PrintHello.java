436ef8ed1d18381e2db730a41e35854b13d122ee
M Sreenivasulu Reddy <smukkidi@gmail.com>
2023-09-05 18:04:06 +0000

    printhello

--- BasicExercises/PrintHello.java
+++ BasicExercises/PrintHello.java
@@ -2,7 +2,8 @@ package BasicExercises;
 public class PrintHello{
   public static void main(String[] args)
   {
-    System.out.println("Hello");
+    System.out.println("Hello world.");
+    System.out.println("This is new line.");
 
   }
 }
